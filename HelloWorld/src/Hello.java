f5cbe77d27310e90c088dcc7db4f6aa6e47217f3
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-13 22:36:30 +0200

    Create Hello.java

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String [] args) {
+        System.out.println("Hello World");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
