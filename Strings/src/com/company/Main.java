0b979d2766b64c6e54b4080c9d4675e37880afb0
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-15 00:07:09 +0200

    Update Main.java

--- Strings/src/com/company/Main.java
+++ Strings/src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String myString = "This is a string";
+        System.out.println(myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        
+        String numberString = "250.55";
+        numberString = numberString + " and 49.95";
+        System.out.println(numberString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
     }
 }
