14b3320bdaca5412890e3159b415504afcf66296
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-14 23:57:11 +0200

    Update Main.java

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -5,5 +5,14 @@ public class Main {
     public static void main(String[] args) {
         //Char can only store 1 character
         char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightCharacter = '\u00A9';
+        System.out.println(myCopyrightCharacter);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
     }
 }
