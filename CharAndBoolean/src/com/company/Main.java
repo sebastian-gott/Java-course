2019a35aa8f5acee6ea81ca056636d74fac1e57f
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-14 23:46:23 +0200

    Update Main.java

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Char can only store 1 character
+        char myChar = 'D';
     }
 }
