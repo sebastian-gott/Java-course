745791d38882dfcf77e78f363ccf4c1783260b97
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-16 00:44:28 +0200

    Update Main.java

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -13,5 +13,21 @@ public class Main {
 
         result = result * 10;
         System.out.println("2 * 10 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien");
+            System.out.println("And this is a test");
+            }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You've got the top score");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than top score and less than 100");
+        }
     }
 }
