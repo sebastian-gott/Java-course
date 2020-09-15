c74677908231a6e2101e797f40ac71a202caea7b
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-15 20:13:05 +0200

    Update Main.java

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println( "previousResult = " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
     }
 }
