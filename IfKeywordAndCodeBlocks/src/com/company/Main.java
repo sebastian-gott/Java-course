761959f3ad60169b3b92872664d3c9fcb426bae5
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-20 23:34:21 +0200

    Update Main.java

--- IfKeywordAndCodeBlocks/src/com/company/Main.java
+++ IfKeywordAndCodeBlocks/src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+
+        if(score == 5000){
+            System.out.println("Your score was " + score);
+        }
     }
 }
