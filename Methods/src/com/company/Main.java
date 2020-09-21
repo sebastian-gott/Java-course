918a905ae709416c2a8b4ba7ecc9cb7c7131ed9a
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-21 03:49:35 +0200

    x

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -3,20 +3,60 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 4000;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        calculateScore(true, 800, 10, 1200);
-        calculateScore(true, 2000, 9, 100);
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        //System.out.println("Your final score was " + highScore);
+
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Seb", highScorePosition);
+
         if (gameOver){
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
+            //System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
         return -1;
+
+
+
+
+
+
+
     }
+
+
+
+
+
+
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table");
+
+    }
+
+    public static int calculateHighScorePosition(int playerScore){
+        if(playerScore > 1000){
+            return  1;
+        } else if (playerScore > 500 && playerScore < 1000 ){
+            return  2;
+        } else if (playerScore > 100 && playerScore < 500){
+            return  3;
+        } else{
+            return  4;
+        }
+    }
+
+
 
 }
