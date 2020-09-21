6c91d4e4e7c3d3033753445519f8e9c3c8f32f6f
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-21 18:38:30 +0200

    Update Main.java

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int newScore = calculateScore("Seb", 500);
+        System.out.println("New score is " + newScore);
+
+    }
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points.");
+        return score * 1000;
     }
 }
