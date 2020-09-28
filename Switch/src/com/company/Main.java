570b204eb59d5aa5eedca4c2aa8de1ccb6983364
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-09-28 13:00:51 +0200

    s

---
+++ Switch/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int value = 1;
+
+        switch(value){
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+    }
+}
