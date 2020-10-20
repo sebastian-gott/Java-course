1f0d5fc7eeffc85a9254f748dd3eb0b40b96536e
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-10-20 11:52:48 +0200

    x

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -3,6 +3,36 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+
+
+        //CReate an array
+        int[] age = {12, 4, 5, 2, 5};
+
+        for(int i = 0; i < age.length; i++) {
+            System.out.println(age[i]);
+        }
+
+        for(int a : age) {
+            System.out.println(a);
+        }
+
+        int[] numbers = {2, -9, 0, 2, 7, 2, 5, 6};
+
+        int sum = 0;
+        double average;
+        //Access all elements with for loop
+        //Adds each element in sum
+        for(int number : numbers) {
+            sum += number;
+        }
+        //Totalt antall elementer
+            int arrayLength = numbers.length;
+        //Kalkuler
+        //Caster verdiene til double
+            average = ((double)sum / (double)arrayLength);
+
+            System.out.println("Sum = " + sum);
+            System.out.println("Average = " + average);
+
     }
 }
