0cf502635efb17f5266776e7fb5211e0ac5fad4c
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2020-11-05 09:16:38 +0100

    hh

--- SwingComponents/src/com/company/Main.java
+++ SwingComponents/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	JFrame aFrame = new JFrame("First frame");
+	aFrame.setSize(1920, 1080);
+	Font headlineFont = new Font("Arial", Font.BOLD, 36);
+	aFrame.setVisible(true);
+	JLabel greeting = new JLabel("Hello");
+	greeting.setFont(headlineFont);
+	aFrame.add(greeting);
+	aFrame.setLocationRelativeTo(null);
+
+
     }
 }
